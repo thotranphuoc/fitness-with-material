4b268618e2b3498094ddb3e6ff1946614c69c194
mac <mac@Jade-MBP.net.fpt>
2018-11-25 22:36:44 +0700

    set max min for datepicker

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -7,10 +7,13 @@ import { NgForm, NgModel } from '@angular/forms';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+  maxDate;
   constructor() { }
 
   ngOnInit() {
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear()-18);
+    console.log(this.maxDate);
   }
 
   submit(f: NgForm, e: NgModel, p: NgModel){
