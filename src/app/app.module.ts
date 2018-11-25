08f381bed6d155f817b0622cd99f11b7bc85acfc
mac <mac@Jade-MBP.net.fpt>
2018-11-25 16:18:07 +0700

    add first material component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '../shares/material.module';
+import { ExampleMaterialComponent } from './example-material/example-material.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleMaterialComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
